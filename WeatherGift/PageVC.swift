9ccd37f9a30951b3588cfbcea2cfbf54f1e1c0f1
Sarah Kim <sarah.mjk@hotmail.com>
2019-03-10 23:15:50 -0400

    7.5

--- WeatherGift/PageVC.swift
+++ WeatherGift/PageVC.swift
@@ -15,6 +15,7 @@ class PageVC: UIPageViewController {
     var currentPage = 0
     var locationsArray = ["Local City", "Syndney, Austrailia", "Accra, Ghana", "Uglich, Russia"]
     var pageControl:UIPageControl!
+    var listButton: UIButton!
     var barButtonWidth: CGFloat = 44
     var barButtonHeight: CGFloat = 44
     
@@ -37,6 +38,7 @@ class PageVC: UIPageViewController {
         super.viewDidAppear(animated)
         
         configurePageControll()
+        configureListButton()
     }
     
     func configurePageControll () {
@@ -48,10 +50,30 @@ class PageVC: UIPageViewController {
         pageControl = UIPageControl (frame: CGRect(x: (view.frame.width - pageControllWidth)/2, y: safeHeight - pageControlHeight, width: pageControllWidth, height: pageControlHeight))
         
         pageControl.pageIndicatorTintColor = UIColor.lightGray
+        pageControl.currentPageIndicatorTintColor = UIColor.black
         pageControl.numberOfPages = locationsArray.count
+        pageControl.currentPage = currentPage
+        pageControl.addTarget(self, action: #selector (pageControlPressed), for: .touchUpInside)
+        
         view.addSubview(pageControl)
         
         
+    }
+    
+    func configureListButton () {
+        let safeHeight = view.frame.height - view.safeAreaInsets.bottom
+        
+        listButton = UIButton(frame: CGRect(x: view.frame.width - barButtonWidth, y: safeHeight - barButtonHeight , width: barButtonWidth, height: barButtonHeight))
+        
+        listButton.setImage(UIImage(named:"listbutton"), for: .normal)
+        listButton.setImage(UIImage(named: "listbuttonhighlighted"), for: .highlighted)
+        listButton.addTarget(self, action: #selector(segueToLocationVC), for: .touchUpInside)
+        view.addSubview(listButton)
+    }
+    
+    @objc func segueToLocationVC () {
+        print ("Hey you clicked me")
+    
     }
     
     
@@ -101,6 +123,17 @@ extension PageVC: UIPageViewControllerDataSource, UIPageViewControllerDelegate {
             }
         
     }
+    
+    @objc func pageControlPressed () {
+        guard let currentViewController = self.viewControllers? [0] as? DetailVC else {return}
+            currentPage = currentViewController.currentPage
+            
+            if pageControl.currentPage < currentPage {
+                setViewControllers([createDetailVC(forPage: pageControl.currentPage)], direction: .reverse, animated: true, completion: nil)
+            }else if pageControl.currentPage > currentPage {
+                setViewControllers([createDetailVC(forPage: pageControl.currentPage)], direction: .forward, animated: true, completion: nil)
+            }
+        }
 }
 
 
