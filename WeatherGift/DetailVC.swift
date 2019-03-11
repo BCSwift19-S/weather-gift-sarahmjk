0a9e46d239494e9aa31536140b40eef784d60a02
Sarah Kim <sarah.mjk@hotmail.com>
2019-03-10 21:23:26 -0400

    7.2

---
+++ WeatherGift/DetailVC.swift
@@ -0,0 +1,30 @@
+//
+//  DetailVC.swift
+//  WeatherGift
+//
+//  Created by Sarah Minji Kim on 3/10/19.
+//  Copyright © 2019 Sarah Minji Kim. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+    
+//outlets
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var currentImage: UIImageView!
+    
+    var currentPage = 0
+    var locationsArray = [String]()
+    
+    
+//viewdidload
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationLabel.text = locationsArray [currentPage]
+    
+    }
+}
